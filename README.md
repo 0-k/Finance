9e53befe828ff3215da87ef59c3b079c870f16fe
0-k <martin.t.klein@gmail.com>
2020-05-30 23:10:35 +0200

    implements FinancialSeries

--- README.md
+++ README.md
@@ -12,12 +12,12 @@
 
 ### Data to consider
 
-* Stock markets: S&P500, Dax, FTSE 100, Hang Seng, Nikkei (5)
+* Stock markets: S&P500, Nasdaq, Dax, FTSE 100, Hang Seng, SSE, Nikkei (7)
 * Volatility: VIX (1)
 * Bond rates (3M, 1Y, 5Y, 10Y) (4)
 * Gold price (1)
 * Oil price (1)
 * Inflation (5Y Breakeven, 10Y Breakeven, 5Y FWD) (3)
-* Currencies: USD-EUR, USD-GBP, USD-CHF, USD-YEN, USD-HKD (5)
+* Currencies: USD-EUR, USD-GBP, USD-CHF, USD-JPY, USD-HKD, USD-CNY (6)
 * Day of the week (5)
 * Month (12)
--- src/FinancialSeries.py
+++ src/FinancialSeries.py
@@ -1,18 +1,54 @@
 import pandas as pd
 import yfinance as yt
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class FinancialSeries:
 
-    def __init__(self):
-        self.values = None
-        self.name = None
-        self.ticker = None
+    def __init__(self, ticker: str, name: str = None, start_date: str = '2003-07-01'):
+        self.ticker = ticker
+        self.closing_values = None
+        self.name = name
+        self.start_date = start_date
 
-    def fetch(self):
-        raise NotImplementedError()
+    def fetch(self, use_cached_values_if_possible=True):
+        if use_cached_values_if_possible:
+            self.__fetch_cached()
+        if self.closing_values is None:
+            self.__fetch_online()
+            self.__cache()
+        return None
 
-    def cache(self):
-        raise NotImplementedError()
+    def __fetch_cached(self):
+        try:
+            self.closing_values = pd.read_hdf('../data/cached/{}.h5'.format(self.ticker))  #
+            return self.closing_values
+        except:
+            print('Warning: Failed to load cached data, will fetch data online')
 
+    def __fetch_online(self):
+        try:
+            ticker = yt.Ticker(self.ticker)
+            self.closing_values = ticker.history(start=self.start_date).Close
+            print(self.closing_values)
+            return self.closing_values
+        except:
+            print('Warning: Could not fetch ' + self.ticker + ' from online sources.')
+
+    def __cache(self):
+        try:
+            self.closing_values.to_hdf('../data/cached/{}.h5'.format(self.ticker), key='data')
+        except:
+            print('Warning: Failed to cache online values.')
+
+    def plot(self):
+        if self.closing_values is not None:
+            plt.plot(self.closing_values)
+            plt.show()
+
+
+if __name__ == '__main__':
+    f = FinancialSeries('GOOGL')
+    f.fetch()
+    f.plot()
