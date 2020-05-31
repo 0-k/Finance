60685ff826a1d4c49cb91ada6e48eeaf1628f895
0-k <martin.t.klein@gmail.com>
2020-05-31 14:37:23 +0200

    initial training data completed

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 class Config:
 
-    indicators = {'S&P_500': '^GSPC',
+    indices = {'S&P_500': '^GSPC',
                   'Nasdaq': '^IXIC',
                   'Dax': '^GDAXI',
                   'FTSE_100': '^FTSE',
@@ -10,28 +10,29 @@ class Config:
                   'Ibovespa': '^BVSP',
                   'Vix': '^VIX',
 
+                  'Gold': 'GC=F',
+               'Silver': 'SI=F',
+               'Copper': 'HG=F',
+               'Heating_Oil': 'HO=F',
+               'Gas': 'NG=F',
+               'Corn': 'C=F',
+               'Lumber': 'LB=F',
+               'Cocoa': 'CC=F',
+               'Cement': 'HEI.DE',
+
+               'USD_EUR': 'EURUSD=X',
+               'USD_GBP': 'GBPUSD=X',
+               'EUR_CHF': 'EURCHF=X',
+               'USD_JPY': 'JPY=X',
+               'USD_HKD': 'HKD=X',
+               'USD_CNY': 'CNY=X',
+               'USD_SGD': 'SGD=X',
+               'USD_INR': 'INR=X',
+               }
+
+    treasuries = {
                   '3M_Treasury': '^IRX',
                   '5Y_Treasury': '^FVX',
                   '10Y_Treasury': '^TNX',
-                  '30Y_Treasury': '^TYX',
-
-                  'Gold': 'GC=F',
-                  'Silver': 'SI=F',
-                  'Copper': 'HG=F',
-                  'Oil': 'CL=F',
-                  'Gas': 'NG=F',
-                  'Corn': 'C=F',
-                  'Lumber': 'LB=F',
-                  'Cocoa': 'CC=F',
-                  'Cement': 'HEI.DE',
-
-                  'USD_EUR': 'EURUSD=X',
-                  'USD_GBP': 'GBPUSD=X',
-                  'EUR_CHF': 'EURCHF=X',
-                  'USD_JPY': 'JPY=X',
-                  'USD_HKD': 'HKD=X',
-                  'USD_CNY': 'CNY=X',
-                  'USD_SGD': 'SGD=X',
-                  'USD_INR': 'INR=X',
-
-                  }
+                  '30Y_Treasury': '^TYX'
+                }
--- src/TrainingData.py
+++ src/TrainingData.py
@@ -7,8 +7,10 @@ from config.config import Config
 
 class TrainingData:
 
-    def __init__(self):
+    def __init__(self, dataset=Config.indices, name='indices'):
         self.values = None
+        self.dataset = dataset
+        self.name = name
 
     def load(self, use_cached_values_if_possible=True):
         if use_cached_values_if_possible:
@@ -19,33 +21,73 @@ class TrainingData:
         return self.values
 
     def __load_cached(self):
-        try:
-            self.values = pd.read_hdf('../data/cached/training/training_data.h5')
-            return self.values
-        except:
-            print('Warning: Failed to load cached data, will fetch data online')
+        if self.name is not None:
+            try:
+                self.values = pd.read_hdf('../data/cached/training/training_data_{}.h5'.format(self.name))
+                return self.values
+            except:
+                print('Warning: Failed to load cached data, will fetch data.')
+        else:
+            try:
+                self.values = pd.read_hdf('../data/cached/training/training_data.h5')
+                return self.values
+            except:
+                print('Warning: Failed to load cached data, will fetch data.')
 
     def __load_series(self):
         self.values = pd.DataFrame()
-        for name in Config.indicators:
+        for key in self.dataset:
             try:
-                ticker = Config.indicators[name]
-                series = FinancialSeries(ticker=ticker, name=name).fetch()
+                ticker = self.dataset[key]
+                series = FinancialSeries(ticker=ticker, name=key).fetch()
                 self.values = pd.concat([self.values, series], axis=1)
             except:
-                print('Loading of series {} unsuccessful.'.format(name))
+                print('Loading of series {} unsuccessful.'.format(key))
         return self.values
 
     def __cache(self):
-        try:
-            self.values.to_hdf('../data/cached/training/training_data.h5', key='data')
-        except:
-            print('Warning: Failed to cache training data values.')
+        if self.name is not None:
+            try:
+                self.values.to_hdf('../data/cached/training/training_data_{}.h5'.format(self.name), key='data')
+            except:
+                print('Warning: Failed to cache training data values.')
+        else:
+            try:
+                self.values.to_hdf('../data/cached/training/training_data.h5', key='data')
+            except:
+                print('Warning: Failed to cache training data values.')
 
-    def print(self):
-        print(self.values)
+    def prepare(self, is_relative=True):
+        self.values.fillna(method='ffill', inplace=True)
+        if is_relative:
+            self.values = self.values.pct_change()
+        else:
+            self.values = self.values.diff()
+
+    def consolidate(self, _with):
+        self.name = None
+        self.values = pd.concat([self.values, _with.values], axis=1)
+        self.values.dropna(inplace=True)
+        self.__cache()
+        return self.values
+
+    def __str__(self):
+        return self.values
 
 
-t = TrainingData()
-t.load()
-t.print()
+def prepare_training_data():
+    indices = TrainingData(dataset=Config.indices, name='indices')
+    indices.load()
+    indices.prepare()
+
+    treasuries = TrainingData(dataset=Config.treasuries, name='treasuries')
+    treasuries.load()
+    treasuries.prepare(is_relative=False)
+    indices.consolidate(_with=treasuries)
+
+
+if __name__ == '__main__':
+    full_training_data = TrainingData(name=None)
+    full_training_data.load()
+    plt.plot(full_training_data.values)
+    plt.show()
